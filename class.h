20174f52347a6af5a46623c09e84c501bbba185d
phanthaikhang21127069 <ptkhang21@clc.fitus.edu.vn>
2022-03-15 15:55:37 +0700

    Create class.h
    file class.h

---
+++ class.h
@@ -0,0 +1,15 @@
+#ifndef _CLASS_H_
+#define _CLASS_H_
+
+#include "Header.h"
+
+// Create class
+
+//void createNewClass(Class*& pClass, char* yearName, char* className, int add);
+
+// Add Student
+
+void input_student(student *&new_student);
+void add_NewStudent(student *&pStu, char* year_name, char* class_name, student *& new_stuedent, int add);
+
+#endif
